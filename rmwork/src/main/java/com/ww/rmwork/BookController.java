098d3d39750ad9cf0bc3144ae9e04693e4453af7
goat <goat@DESKTOP-CGCDRI9>
2022-12-26 02:42:56 +0900

    test commit

--- rmwork/src/main/java/com/ww/rmwork/BookController.java
+++ rmwork/src/main/java/com/ww/rmwork/BookController.java
@@ -1,5 +1,6 @@
 package com.ww.rmwork;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,11 +15,18 @@ public class BookController {
 	
 	@Autowired
 	BookService bookService;
+	
+
+	@RequestMapping(value="/")
+	public ModelAndView home() {
+		return new ModelAndView("/home");
+	}
 
 	
 	/*
 	 * 	브라우저 주소가 create 일 때 실행되는 자바 컨트롤 메소드
 	 * 	https://wikidocs.net/115240
+	 * 	book/create.jsp 로 랜딩
 	 */
 	@RequestMapping(value="/create", method = RequestMethod.GET)
 	public ModelAndView create() {
@@ -26,6 +34,12 @@ public class BookController {
 	}
 
 	
+	/*
+	 * 	서비스를 이용해서 책 추가하는 메소드
+	 * 	dao - insert
+	 * 	service - create
+	 * 	https://wikidocs.net/115247
+	 */
 	@RequestMapping(value = "/create", method = RequestMethod.POST)
 	public ModelAndView createPost(@RequestParam Map<String, Object> map) {
 	    ModelAndView mav = new ModelAndView();
@@ -39,6 +53,106 @@ public class BookController {
 
 	    return mav;
 	}
+	
+	
+	/*
+	 * 	https://wikidocs.net/115251
+	 * 	책 상세 url 입력시 실행
+	 */
+	@RequestMapping(value="/detail", method=RequestMethod.GET)
+	public ModelAndView detail(@RequestParam Map<String, Object> map) {
+		Map<String, Object> detailMap = this.bookService.detail(map);
+		
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("data", detailMap);
+		String bookId = map.get("bookId").toString();
+		mav.addObject("bookId", bookId);
+		mav.setViewName("/book/detail");
+		
+		return mav;
+	}
+	
+	
+	/*
+	 * 	책 수정 화면으로 이동
+	 * 	https://wikidocs.net/117265
+	 */
+	@RequestMapping(value = "/update", method = RequestMethod.GET)
+	public ModelAndView update(@RequestParam Map<String, Object> map) {
+	    Map<String, Object> detailMap = this.bookService.detail(map);
+	    
+	    ModelAndView mav = new ModelAndView();
+	    mav.addObject("data", detailMap);
+	    mav.setViewName("/book/update");
+	    return mav;
+	}
+	
+	
+	/*
+	 * 	책 수정
+	 * 	https://wikidocs.net/117267
+	 * 
+	 * 	bookid 는 get 파라미터로 전달
+	 * 	나머지 title, category, price 는 data 객체로 form 태그를 통해 전달
+	 * 
+	 * 	스프링은 http 메소드를 get post 가리지 않고 requestmapping 어노테이션이 있으면 무조건 파라미터를 넣어줌
+	 * 	파라미터 map 안에는 4개 데이터가 모두 들어있음
+	 */
+	@RequestMapping(value = "update", method = RequestMethod.POST)
+	public ModelAndView updatePost(@RequestParam Map<String, Object> map) {
+	    ModelAndView mav = new ModelAndView();
+
+	    boolean isUpdateSuccess = this.bookService.edit(map);		
+	    if (isUpdateSuccess) {
+	        String bookId = map.get("bookId").toString();
+	        mav.setViewName("redirect:/detail?bookId=" + bookId);
+	    }else {
+	        mav = this.update(map);
+	    }		
+
+	    return mav;
+	}
+	
+	
+	/*
+	 * 	책 삭제
+	 * 	https://wikidocs.net/117271
+	 */
+	@RequestMapping(value="/delete", method=RequestMethod.POST)
+	public ModelAndView deletePost(@RequestParam Map<String, Object> map) {
+		ModelAndView mav = new ModelAndView();
+		
+		boolean isDeleteSuccess = this.bookService.remove(map);
+		if (isDeleteSuccess) {
+			mav.setViewName("redirect:/list");
+		}
+		else {
+			String bookId = map.get("bookId").toString();
+			mav.setViewName("redirect:/detail?bookId="+bookId);
+		}
+		
+		return mav;
+	}
+	
+	
+	/*
+	 * 	책 목록
+	 * 	https://wikidocs.net/117273
+	 */
+	@RequestMapping(value="list")
+	public ModelAndView list(@RequestParam Map<String, Object> map) {
+		List<Map<String, Object>> list = this.bookService.list(map);
+		
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("data", list);
+		mav.setViewName("/book/list");
+		
+		return mav;
+	}
+	
+	
+	
+	
 	
 	
 }
--- rmwork/src/main/java/com/ww/rmwork/BookServiceImpl.java
+++ rmwork/src/main/java/com/ww/rmwork/BookServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ww.rmwork;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,9 @@ public class BookServiceImpl implements BookService {
 	@Autowired
 	BookDao bookDao;
 
+	/*
+	 * 	책 추가
+	 */
 	@Override
 	public String create(Map<String, Object> map) {
 	    int affectRowCount = this.bookDao.insert(map);
@@ -19,5 +23,53 @@ public class BookServiceImpl implements BookService {
 	    }
 	    return null;	    
 	}
+	
+	
+	/*
+	 * 	책 상세
+	 */
+	@Override
+	public Map<String, Object> detail(Map<String, Object> map){
+	    return this.bookDao.selectDetail(map);
+	}
+	
+	
+	/*
+	 * 	책 수정
+	 * 	https://wikidocs.net/117266
+	 * 	1개의 행이 영향을 받았는지 확인 (return)
+	 */
+	@Override
+	public boolean edit(Map<String, Object> map) {
+		int affectRowCount = this.bookDao.update(map);
+		return affectRowCount == 1;	
+	}
+	
+	
+	/*
+	 * 	책 삭제
+	 * 	https://wikidocs.net/117271
+	 */
+	@Override
+	public boolean remove(Map<String, Object> map) {
+	    int affectRowCount = this.bookDao.delete(map);
+	    return affectRowCount == 1;	    
+	}
+	
+	
+	/*
+	 * 	책 목록
+	 * 	https://wikidocs.net/117272
+	 */
+	@Override
+	public List<Map<String, Object>> list(Map<String, Object> map) {
+		return this.bookDao.selectList(map);
+	}
+	
+	
+	
+	
+	
+	
 	
 }
